e21c3c9cf7e5cbfbb18241c7566ce3fe1a4a0207
genericdefaultentity <genericdefaultentity@2e89b3da-aad9-11de-9f33-a79f98c9f2a6>
2010-08-05 06:31:37 +0000

    Added iron sights FAL.

--- gdcw/lua/weapons/gdcw_faliron/shared.lua
+++ gdcw/lua/weapons/gdcw_faliron/shared.lua
@@ -3,16 +3,16 @@ SWEP.Category				= "Generic Default's Weapons"
 SWEP.Author				= "Generic Default"
 SWEP.Contact				= "AIDS!"
 SWEP.Purpose				= "The best gun there is."
-SWEP.Instructions				= "Round: 7.62x51 \nVelocity: ~840 m/s \nSights: Illuminated NVG 6x \nCapacity: 30 rounds \nRate of Fire: 700 rounds per minute \nPress TAB while scoped to active night vision."
+SWEP.Instructions				= "Round: 7.62x51 \nVelocity: ~840 m/s \nSights: Iron \nCapacity: 30 rounds \nRate of Fire: 700 rounds per minute"
 SWEP.MuzzleAttachment			= "1" 	-- Should be "1" for CSS models or "muzzle" for hl2 models
 SWEP.ShellEjectAttachment			= "2" 	-- Should be "2" for CSS models or "1" for hl2 models
 SWEP.DrawCrosshair			= false	
 
 SWEP.ViewModelFOV			= 60
 SWEP.ViewModelFlip			= true
-SWEP.ViewModel				= "models/weapons/v_FAL_SNIPE.mdl"
+SWEP.ViewModel				= "models/weapons/v_GDCIROFAL.mdl"
 SWEP.WorldModel				= "models/weapons/w_GDCFAL.mdl"
-SWEP.Base 				= "gdcw_base_rifleman"
+SWEP.Base 				= "gdcw_base_assault"
 SWEP.Spawnable				= true
 SWEP.AdminSpawnable			= true
 
@@ -21,9 +21,9 @@ SWEP.Primary.Round			= ("gdcwa_7.62x51_tracer")
 SWEP.Primary.RPM				= 700					// This is in Rounds Per Minute
 SWEP.Primary.ClipSize			= 30					// Size of a clip
 SWEP.Primary.DefaultClip			= 31					// Default number of bullets in a clip
-SWEP.Primary.KickUp			= 0.55						// Maximum up recoil (rise)
-SWEP.Primary.KickDown			= 0.5						// Maximum down recoil (skeet)
-SWEP.Primary.KickHorizontal			= 0.4						// Maximum up recoil (stock)
+SWEP.Primary.KickUp			= 0.55					// Maximum up recoil (rise)
+SWEP.Primary.KickDown			= 0.5					// Maximum down recoil (skeet)
+SWEP.Primary.KickHorizontal			= 0.4					// Maximum up recoil (stock)
 SWEP.Primary.Automatic			= true					// Automatic/Semi Auto
 SWEP.Primary.Ammo			= "ar2"
 
@@ -31,18 +31,17 @@ SWEP.Secondary.ClipSize			= 1					// Size of a clip
 SWEP.Secondary.DefaultClip			= 1					// Default number of bullets in a clip
 SWEP.Secondary.Automatic			= false					// Automatic/Semi Auto
 SWEP.Secondary.Ammo			= ""
-SWEP.Secondary.ScopeZoom			= 6	
-SWEP.Secondary.UseGreenDuplex		= true	
+SWEP.Secondary.IronFOV			= 40	
+
 
 SWEP.data 				= {}					-- The starting firemode
 SWEP.data.ironsights			= 1
-SWEP.ScopeScale 				= 0.6
-SWEP.ReticleScale 				= 1.4
-SWEP.NVG				= false	
 
-SWEP.IronSightsPos = Vector (3.2269, -3.3734, 1.3315)
-SWEP.IronSightsAng = Vector (0, 0, 0)
-SWEP.SightsPos = Vector (3.2269, -3.3734, 1.3315)
-SWEP.SightsAng = Vector (0, 0, 0)
+
+
+SWEP.IronSightsPos = Vector (2.854, -2.2612, 1.5607)
+SWEP.IronSightsAng = Vector (-2.623, 0.9674, 0)
+SWEP.SightsPos = Vector (2.854, -2.2612, 1.5607)
+SWEP.SightsAng = Vector (-2.623, 0.9674, 0)
 SWEP.RunSightsPos = Vector (-1.502, -1.2016, 2.6582)
 SWEP.RunSightsAng = Vector (-28.3988, -39.7767, 10)
