c6d63427dcaf91363c4fcef44daaa0895cab20a2
genericdefaultentity <genericdefaultentity@2e89b3da-aad9-11de-9f33-a79f98c9f2a6>
2010-08-14 04:12:19 +0000

    Fixed name changed 30mm shells.

---
+++ gdc/lua/entities/gdca_30x165_he/init.lua
@@ -0,0 +1,67 @@
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include('shared.lua')
+
+function ENT:Initialize()   
+
+math.randomseed(CurTime())
+self.flightvector = self.Entity:GetUp() * 500
+self.timeleft = CurTime() + 5
+self.Entity:SetModel( "models/combatmodels/tankshell_40mm.mdl" ) 	
+self.Entity:PhysicsInit( SOLID_VPHYSICS )      -- Make us work with physics,  	
+self.Entity:SetMoveType( MOVETYPE_NONE )   --after all, gmod is a physics  	
+self.Entity:SetSolid( SOLID_VPHYSICS )        -- CHEESECAKE!    >:3            
+
+self:Think()
+ 
+end   
+
+function ENT:Think()
+
+		if self.timeleft < CurTime() then
+		self.Entity:Remove()			
+		end
+
+	local trace = {}
+		trace.start = self.Entity:GetPos()
+		trace.endpos = self.Entity:GetPos() + self.flightvector 
+		trace.filter = self.Entity 
+	local tr = util.TraceLine( trace )
+	
+
+			if tr.HitSky then
+			self.Entity:Remove()
+			return true
+			end
+
+				if (tr.Hit) then
+					util.BlastDamage(self.Entity, self.Entity, tr.HitPos, 300, 100)
+					local effectdata = EffectData()
+					effectdata:SetOrigin(tr.HitPos)
+					effectdata:SetNormal(tr.HitNormal)
+					effectdata:SetScale(1.5)			// Size of cloud
+					effectdata:SetRadius(1)			// Size of ring
+					effectdata:SetMagnitude(40)			// Size of flash
+					util.Effect( "gdca_splodecolumn", effectdata )
+					util.ScreenShake(tr.HitPos, 10, 5, 0.5, 1000 )
+					util.Decal("Scorch", tr.HitPos + tr.HitNormal, tr.HitPos - tr.HitNormal)
+
+					if (tr.Entity:IsValid()) then
+					cbt_hcgexplode( tr.HitPos, 40, 100, 10)				// Radius, Damage
+					local attack = cbt_dealhcghit( tr.Entity, 200, 15, tr.HitPos , tr.HitPos)	// Ent, Damage, Pierce				
+					end
+
+					self.Entity:Remove()
+					end
+
+
+	self.Entity:SetPos(self.Entity:GetPos() + self.flightvector)
+	self.flightvector = self.flightvector + Vector(math.Rand(-0.40,0.40), math.Rand(-0.4,0.4),math.Rand(-0.4,0.4)) + Vector(0,0,-0.2)
+	self.Entity:SetAngles(self.flightvector:Angle() + Angle(90,0,0))
+	self.Entity:NextThink( CurTime() )
+	return true
+	end
+ 
+ 
