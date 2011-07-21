6595c3abd2bf7a97a2374f91d22359791041d2ef
genericdefaultentity <genericdefaultentity@2e89b3da-aad9-11de-9f33-a79f98c9f2a6>
2011-07-21 03:47:00 +0000

    Added a 40mm nade for those of you who work on your own guns. Nothing new for standard GDCW.

---
+++ gdcw/lua/entities/gdcwa_40x49_he/init.lua
@@ -0,0 +1,65 @@
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include('shared.lua')
+
+function ENT:Initialize()   
+self.Owner = self:GetOwner()
+self.flightvector = self.Entity:GetUp() * ((75*39.37)/66)             	-- Velocity in m/s, inches to meters conversion, ticks per second.FIRST NUMMER = SPEED
+self.timeleft = CurTime() + 25
+self.Entity:SetModel( "models/led2.mdl" )
+self.Entity:PhysicsInit( SOLID_VPHYSICS )      -- Make us work with physics,  	
+self.Entity:SetMoveType( MOVETYPE_NONE )   --after all, gmod is a physics  	
+self.Entity:SetSolid( SOLID_VPHYSICS )        -- CHEESECAKE!    >:3           
+self.Entity:SetColor(0,0,0,255)
+ 
+end   
+
+ function ENT:Think()
+	
+
+		if self.timeleft < CurTime() then
+		self.Entity:Remove()				
+		end
+
+	Table	={} 			//Table name is table name
+	Table[1]	=self.Owner 		//The person holding the gat
+	Table[2]	=self.Entity 		//The cap
+
+	local trace = {}
+		trace.start = self.Entity:GetPos()
+		trace.endpos = self.Entity:GetPos() + self.flightvector
+		trace.filter = Table
+	local tr = util.TraceLine( trace )
+	
+
+			if tr.HitSky then
+			self.Entity:Remove()
+			return true
+			end
+	
+				if tr.Hit then
+					util.BlastDamage(self.Entity, self.Entity, tr.HitPos, 400, 250)		// 10m Splosion
+					local effectdata = EffectData()
+					effectdata:SetOrigin(tr.HitPos)				// Position of Impact
+					effectdata:SetNormal(tr.HitNormal)			// Direction of Impact
+					effectdata:SetStart(self.flightvector:GetNormalized())	// Direction of Round
+					effectdata:SetEntity(self.Entity)			// Who done it?
+					effectdata:SetScale(1.5)				// Size of explosion
+					effectdata:SetRadius(tr.MatType)			// Texture of Impact
+					effectdata:SetMagnitude(16)				// Length of explosion trails
+					util.Effect( "gdcw_cinematicboom", effectdata )
+					util.ScreenShake(tr.HitPos, 10, 5, 1, 2000 )
+					util.Decal("Scorch", tr.HitPos + tr.HitNormal, tr.HitPos - tr.HitNormal)
+					self.Entity:Remove()	
+					end
+	
+	self.Entity:SetPos(self.Entity:GetPos() + self.flightvector)
+	self.flightvector = self.flightvector + Vector(math.Rand(-0.05,0.05), math.Rand(-0.05,0.05),math.Rand(-0.05,0.05)) + Vector(0,0,-0.111)
+	self.Entity:SetAngles(self.flightvector:Angle() + Angle(90,0,0))
+	self.Entity:NextThink( CurTime() )
+	return true
+end
+ 
+ 
