70d0bbcd1b8354aebe3ad92760d443356d2dad7d
genericdefaultentity <genericdefaultentity@2e89b3da-aad9-11de-9f33-a79f98c9f2a6>
2011-09-03 01:07:55 +0000

    Fixed a problem with scopes not downloading, Capital W!

--- gdcss/lua/autorun/server/gdcw_css_scope.lua
+++ gdcss/lua/autorun/server/gdcw_css_scope.lua
@@ -3,16 +3,16 @@
 ------------------------------------------------------------------------------------------------------------------*/
 
 
-resource.AddFile( "materials/scope/gdcW_acog.vmf" );
-resource.AddFile( "materials/scope/gdcW_acog.vtf" );
-resource.AddFile( "materials/scope/gdcW_acogchevron.vmf" );
-resource.AddFile( "materials/scope/gdcW_acogchevron.vtf" );
-resource.AddFile( "materials/scope/gdcW_acogcross.vmf" );
-resource.AddFile( "materials/scope/gdcW_acogcross.vtf" );
-resource.AddFile( "materials/scope/gdcW_closedsight.vmf" );
-resource.AddFile( "materials/scope/gdcW_closedsight.vtf" );
-resource.AddFile( "materials/scope/gdcW_parabolicsight.vmf" );
-resource.AddFile( "materials/scope/gdcW_parabolicsight.vtf" );
-resource.AddFile( "materials/scope/gdcW_scopesight.vmf" );
-resource.AddFile( "materials/scope/gdcW_scopesight.vtf" );
-resource.AddFile( "materials/scope/gdcW_noise.vtf" );
+resource.AddFile( "materials/scope/gdcw_acog.vmf" );
+resource.AddFile( "materials/scope/gdcw_acog.vtf" );
+resource.AddFile( "materials/scope/gdcw_acogchevron.vmf" );
+resource.AddFile( "materials/scope/gdcw_acogchevron.vtf" );
+resource.AddFile( "materials/scope/gdcw_acogcross.vmf" );
+resource.AddFile( "materials/scope/gdcw_acogcross.vtf" );
+resource.AddFile( "materials/scope/gdcw_closedsight.vmf" );
+resource.AddFile( "materials/scope/gdcw_closedsight.vtf" );
+resource.AddFile( "materials/scope/gdcw_parabolicsight.vmf" );
+resource.AddFile( "materials/scope/gdcw_parabolicsight.vtf" );
+resource.AddFile( "materials/scope/gdcw_scopesight.vmf" );
+resource.AddFile( "materials/scope/gdcw_scopesight.vtf" );
+resource.AddFile( "materials/scope/gdcw_noise.vtf" );
