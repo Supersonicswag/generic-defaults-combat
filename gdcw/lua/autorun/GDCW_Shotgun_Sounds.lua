1b0f994cf8dc619170184724254aca9efc3d6d6b
genericdefaultentity <genericdefaultentity@2e89b3da-aad9-11de-9f33-a79f98c9f2a6>
2012-11-20 05:00:47 +0000

    Added shotgun sound script. The shotgun pellets are grouping weirdly, it looks like Garry screwed up FOR loops....

---
+++ gdcw/lua/autorun/GDCW_Shotgun_Sounds.lua
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////
+sound.Add(
+{
+    name = "M3F.single",
+    channel = CHAN_WEAPON,
+    volume = 1.0,
+    soundlevel = SNDLVL_150dB,
+    sound = "GDC/Shotgun/ShotgunBlast.wav"
+})
+sound.Add(
+{
+    name = "GenericM3.InsertShell",
+    channel = CHAN_RELOAD,
+    volume = 1.0,
+    soundlevel = SNDLVL_80dB,
+    sound = "GDC/Shotgun/ShotgunShell.wav"
+})
+sound.Add(
+{
+    name = "GenericM3.Pump",
+    channel = CHAN_RELOAD,
+    volume = 1.0,
+    soundlevel = SNDLVL_80dB,
+    sound = "GDC/Shotgun/ShotgunPump.wav"
+})
